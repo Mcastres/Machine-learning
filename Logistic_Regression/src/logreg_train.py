efdd3e00c690d0a376e9eaadddc9c8d788ab5efa
Sebastien ALIBERT <salibert@e1r1p12.42.fr>
2019-02-11 18:08:52 +0100

    [WIP] logreg

--- Logistic_Regression/src/logreg_train.py
+++ Logistic_Regression/src/logreg_train.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.preprocessing import Imputer
 import sys
 import math
-from sklearn import datasets, linear_model
 
 # Read the csv dataset and return an array of it
 def read_csv_file(file):
@@ -16,55 +15,57 @@ def read_csv_file(file):
     x = imputer.transform(x)
     return [x, y]
 
-class LogisticRegression(object):
+def loss( h , y, m):
+    return sum(y * np.log(h) + (1 - y) * np.log(1 - h)) / -m
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+class LogisticRegression(object):
+ 
     def __init__(self, learning_rate=0.001, n_iter=50):
         self.learning_rate = learning_rate
         self.n_iter = n_iter
 
     def fit(self, X, y):
         X = np.insert(X, 0, 1, axis=1)
-        self.w_ = []
+        self.weight = []
 
         for i in np.unique(y):
-            y_copy = [1 if c == i else 0 for c in y]
+            y_copy = np.array([1 if c == i else 0 for c in y])
+            m = y_copy.shape[0]
             w = np.ones(X.shape[1])
-            print(w)
-            # print('training ', i)
-            # counter = 0
-
+            cost = 0
             for _ in range(self.n_iter):
-                output = X.dot(w)
-                errors = y_copy - self.sigmoid(output)
-                w += self.learning_rate * errors.T.dot(X)
-                
-                # counter += 1
-                # if counter // 10 == 0:
-                #     print(sum(errors**2) / 2.0)
-            self.w_.append((w, i))
-
+                h = sigmoid(X.dot(w))
+                cost = loss(h, y_copy, m)
+                w -= self.learning_rate * ((h - y_copy).dot(X) / m)
+            print(cost)
+            self.weight.append((w, i))
         return self
 
     def getW(self):
-        return self.w_
+        return self.weight
 
-    def predictOne(self, x):
-        return max((x.dot(w), c) for w, c in self.w_)[1]
+    def predictOne(self, x):    
+        return max((x.dot(w), c) for w, c in self.weight)[1]
 
     def predict(self, X):
         return np.array([self.predictOne(i) for i in np.insert(X, 0, 1, axis=1)])
 
-    def sigmoid(self, x):
-        return 1.0 / (1 + np.exp(-x))
-
 [X, Y] = read_csv_file(sys.argv[1])
-
-logreg = LogisticRegression(n_iter=1000)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X = sc.fit_transform(X)
 
-logreg.fit(X, Y)
-# print(logreg.getW())
-# Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+logreg = LogisticRegression(n_iter=1000)
+
+logreg.fit(X_train, y_train)
+print(logreg.getW())
+y_pred = logreg.predict(X_test)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(y_test, y_pred))
